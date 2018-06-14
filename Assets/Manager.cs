2528c3cab9cac03340001cce371fe4a1e83a44ad
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2018-06-14 18:32:40 -0300

    PingPongMaterials

---
+++ Assets/Manager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour 
+{
+	public GameObject ball;
+	void Start () 
+	{
+		
+	}	
+	void Update () 
+	{
+		var deviceIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
+		if (deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressDown (Valve.VR.EVRButtonId.k_EButton_A)) 
+		{
+			SteamVR_LoadLevel ("PingPong");
+		}
+		if (deviceIndex != -1 && SteamVR_Controller.Input (deviceIndex).GetPressDown (Valve.VR.EVRButtonId.k_EButton_DPad_Down)) 
+		{
+			SteamVR_LoadLevel ("PingPong");
+		}
+	}
+}
